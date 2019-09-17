a119c19de7515c41ff294a91e827ef9e918e35ac
Alexander Kartsev <alexkartsev1994@gmail.com>
2019-09-17 17:11:20 +0300

    init

---
+++ PotatoWeigher/DeeplabModel.h
@@ -0,0 +1,18 @@
+//
+//  DeeplabModel.h
+//  Segmentation Nails
+//
+//  Created by Alexey Korotkov on 8/26/19.
+//  Copyright © 2019 Alexey Korotkov. All rights reserved.
+//
+
+#include <stdio.h>
+
+#import <AVFoundation/AVFoundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DeepLabModel : NSObject
+@property (strong, nonatomic) NSMutableArray *lastCordinates;
+- (unsigned char *) process:(CVPixelBufferRef)pixelBuffer additionalColor:(unsigned int)additionalColor;
+- (BOOL) loadModel;
+@end
